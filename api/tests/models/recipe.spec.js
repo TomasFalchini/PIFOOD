3c7c6ca4c7f4d6e0ddec7c66fd7ea6a939cc504a
Tomas Falchini <tomasfalchini@gmail.com>
2022-10-11 01:48:14 -0300

    test model Recipe and Routes

--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -1,22 +1,91 @@
-const { Recipe, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Recipe, conn } = require("../../src/db.js");
+const { expect } = require("chai");
 
-describe('Recipe model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
-    beforeEach(() => Recipe.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Recipe.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
-      });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Milanesa a la napolitana' });
-      });
+describe("Recipe Model", () => {
+  before(async () => {
+    await conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
     });
+    await Recipe.sync({ force: true });
+  });
+
+  it("should not create the Recipe if name is not send", async () => {
+    try {
+      await Recipe.create({});
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should not create the Recipe if resume is not send", async () => {
+    try {
+      await Recipe.create({ name: "Milanesa a la napolitana" });
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should not create the Recipe if health score is not a integer", async () => {
+    try {
+      await Recipe.create({
+        name: "Milanesa a la napolitana",
+        resume: "Rico alimento tradicional de la República Argentina",
+        health_score: "Esto no va",
+      });
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should not create the Recipe if name is not valid", async () => {
+    try {
+      await Recipe.create({
+        name: "Milanesas a la napolitana ==!%#***",
+        resume: "Rico alimento tradicional de la República Argentina",
+      });
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should not create the Recipe if steps is not string", async () => {
+    try {
+      await Recipe.create({
+        name: "Milanesa a la napolitana",
+        resume: "Rico alimento tradicional de la República Argentina",
+        health_score: 77,
+        steps: 15,
+      });
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should not create the Recipe if health score is not between 0 and 100", async () => {
+    try {
+      await Recipe.create({
+        name: "Milanesa a la napolitana",
+        resume: "Rico alimento tradicional de la República Argentina",
+        health_score: 110,
+      });
+    } catch (error) {
+      expect(error).to.have.property("message");
+    }
+  });
+
+  it("should create the Recipe if all required properties are ok", async () => {
+    const recipe = await Recipe.create({
+      name: "Milanesa a la napolitana",
+      resume: "Rico alimento tradicional de la República Argentina",
+      health_score: 99,
+      steps: "Step Number 1: .....",
+      image: "image URL ...",
+      created: true,
+    });
+    expect(recipe.toJSON()).to.be.a("object");
+    expect(recipe.toJSON()).to.have.property("name");
+    expect(recipe.toJSON()).to.have.property("ID");
+    expect(recipe.toJSON()).to.have.property("resume");
+    expect(recipe.toJSON()).to.have.property("image");
   });
 });
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,24 +1,77 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Recipe, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Recipe, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  name: "Milanesa a la napolitana",
+  resume: "Rica comida gourmet argentina",
 };
 
-describe('Recipe routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Recipe.sync({ force: true })
-    .then(() => Recipe.create(recipe)));
-  describe('GET /recipes', () => {
-    it('should get 200', () =>
-      agent.get('/recipes').expect(200)
-    );
+describe("Recipe routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() =>
+    Recipe.sync({ force: true }).then(() => Recipe.create(recipe))
+  );
+  describe("GET /recipes", function () {
+    this.timeout(10000);
+    it("should get 200", () => agent.get("/recipes").expect(200));
+  });
+  describe("GET /diets", function () {
+    this.timeout(10000);
+    it("should get 200", () => agent.get("/diets").expect(200));
+  });
+  describe("GET /recipes?name=Milanesa", function () {
+    this.timeout(10000);
+    it("should get 200", () => agent.get("/recipes?name=Milanesa").expect(200));
+  });
+  describe("GET /recipes?name=undefined", function () {
+    this.timeout(10000);
+    it("should get 404 if the recipe name does not match", () =>
+      agent.get("/recipes?name=undefined").expect(404));
+  });
+  describe("GET /recipes/:idRecipe", function () {
+    this.timeout(10000);
+    it("should get 404 if the ID does not match with a recipe", () =>
+      agent.get("/recipes/23298329048hfskdjfsdjf").expect(404));
+  });
+  describe("GET /recipes/:idRecipe", function () {
+    this.timeout(10000);
+    it("should get 200 if the ID  match with a recipe", async () => {
+      const recipe = await Recipe.findAll({
+        where: {
+          name: "Milanesa a la napolitana",
+        },
+      });
+      const id = recipe.ID;
+      agent.get(`/recipes/${id}`).expect(200);
+    });
+  });
+  describe("POST /recipes/create", function () {
+    this.timeout(10000);
+    it("should get 404 if resume is missing in the post BODY", () =>
+      agent
+        .post("/recipes/create")
+        .send({ name: "Milanesas con pure" })
+        .expect(404));
+  });
+  describe("POST /recipes/create", function () {
+    this.timeout(10000);
+    it("should get 404 if name is missing in the post BODY", () =>
+      agent.post("/recipes/create").send({ resume: "Muy ricas" }).expect(404));
+  });
+  describe("POST /recipes/create", function () {
+    this.timeout(10000);
+    it("should get 200 if name and resume are in the post BODY", () =>
+      agent
+        .post("/recipes/create")
+        .send({ name: "Milanesas con pure", resume: "Muy ricas" })
+        .expect(200));
   });
 });
